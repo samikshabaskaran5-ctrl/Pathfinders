712274c0071a279c1a42a18c065f1ffc766a9513
samikshabaskaran5-ctrl <samikshabaskaran5@gmail.com>
2025-11-22 17:50:47 +0530

    Added python file

---
+++ Day 6/Hollow Triangle.py
@@ -0,0 +1,9 @@
+n = int(input("Enter a number: "))
+
+for i in range(1, n + 1):
+    if i == 1:
+        print("*")                 
+    elif i == n:
+        print("*" * n)             
+    else:
+        print("*" + " " * (i - 2) + "*")   
